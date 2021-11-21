d83903863263eff8c7825b950f9839f259b73189
CarlossViniciuss <carlosvinicius.index@gmail.com>
2021-11-21 09:18:11 -0300

    new

--- bodytest/resources/actions/libs/DeloreanLibrary.py
+++ bodytest/resources/actions/libs/DeloreanLibrary.py
@@ -1,18 +1,40 @@
 import psycopg2
-
+from logging import info
 class DeloreanLibrary():
 
-	# No Robot Vira uma Keyword automaticamente -> Remove Student   email@desejado
-    def remove_student(self, email):
-
-        query = "delete from students where email = '{}'".format(email)
-
-        conn = psycopg2.connect(
+    def connect(self):
+        return psycopg2.connect(
             host='ec2-18-209-153-180.compute-1.amazonaws.com', 
             database='da55naurer6de6',
             user='wvxegdesuajiof',
             password='0c19e04562d6f3e679639baf8baf57c1ec7e7ea871199f4f3e6803c9078e7dfc'
         )
+
+	# No Robot Vira uma Keyword automaticamente -> Remove Student   email@desejado
+    def remove_student(self, email):
+
+        query = "delete from students where email = '{}'".format(email)
+        info(query)
+
+        conn = self.connect()
+
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        conn.close()
+
+    def insert_student(self, student):
+
+        self.remove_student(student['email'])
+
+        query = ("insert into students (name, email, age, weight, feet_tall, created_at, updated_at)"
+            "values('{}','{}',{},{},{}, NOW(), NOW());"
+            .format(student['name'], student['email'],student['age'],student['weight'], student['feet_tall'])
+        )
+
+        info(query)
+
+        conn = self.connect()
 
         cur = conn.cursor()
         cur.execute(query)
--- bodytest/tests/cadastro_alunos.robot
+++ bodytest/tests/cadastro_alunos.robot
@@ -7,6 +7,7 @@ Suite Setup         Start Admin Session
 
 ***Test Cases***
 Novo aluno
+
     &{student}      Create Dictionary       name=Carlos Vinicius    email=carlos@teste.com      age=20      weight=80       feet_tall=1.85
 
     Remove Student                  ${student.email} 
@@ -14,5 +15,19 @@ Novo aluno
     Go To Form Student
     New Student                     ${student}
     Toaster Text Should Be          Aluno cadastrado com sucesso.
+
+    [Teardown]                      Thinking And Take Screenshot    2
+
+Não deve permitir email duplicado
+    [tags]          dup
+
+    &{student}      Create Dictionary       name= Vinicius    email=vinicius@teste.com      age=21      weight=85       feet_tall=1.80
+
+    Insert Student                  ${student}
+
+    Go To Students
+    Go To Form Student
+    New Student                     ${student}
+    Toaster Text Should Be          Email já existe no sistema.
 
     [Teardown]                      Thinking And Take Screenshot    2
